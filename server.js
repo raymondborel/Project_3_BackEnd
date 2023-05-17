b8ad4b17b700f7a6deb6c9875006e758f51db915
larrytu327 <larrytu327@gmail.com>
2023-05-16 19:18:30 -0700

    setup server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// Import Dependencies
+const express = require("express");
+const cors = require("cors");
+
+// Create our app object
+const app = express();
+
+// set up middleware
+app.use(cors());
+
+//home route for testing our app
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// route for retrieving projects
+app.get("/restaurants", (req, res) => {
+  // send projects via JSON
+});
+
+//declare a variable for our port number
+const PORT = process.env.PORT || 4000;
+
+// turn on the server listener
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
